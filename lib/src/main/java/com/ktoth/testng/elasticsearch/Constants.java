a3020b51c42d06666a115e46a7566b41a1f1065e
Tóth Krisztián Gyula <t.krisztian95@gmail.com>
2021-02-23 17:11:50 +0100

    Changes access modifier to package private

--- lib/src/main/java/com/ktoth/testng/elasticsearch/Constants.java
+++ lib/src/main/java/com/ktoth/testng/elasticsearch/Constants.java
@@ -1,6 +1,6 @@
 package com.ktoth.testng.elasticsearch;
 
-public class Constants {
+class Constants {
     public static final String INDEX_MAPPING_TEST_RESULTS = "testng-test-result";
     public static final String INDEX_MAPPING_TEST_RUNS = "testng-test-run";
 }
